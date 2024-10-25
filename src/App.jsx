1d517fd102b0be4ed5e985174382ff64ce4afd87
Nguyen Duy Linh <nguyenduylinh0199@gmail.com>
2024-10-25 14:21:55 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+         <h1>Welcome To Arey Store</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
          
@@ -40,6 +40,5 @@ function App() {
 }
 
 export default App;
-
 
 
